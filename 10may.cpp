c5cfcd05e5ba86316a33a2aecbe0cec13097f1d8
Natasha Wanja <kinuthianatasha@gmail.com>
2023-05-10 13:44:00 +0300

    update

--- 10may.cpp
+++ 10may.cpp
@@ -2,15 +2,27 @@
 using namespace std;
 
 int main(){
-    int arr[2] [3] = {{1,2,3},{4,5,6}};
-
-    for (int i = 0; i < arr; i++)
+    int arr[2] [3] = {{},{}};
     {
-       sizeof (arr);
-       cout<<"arr";
+     int row = sizeof(arr)/ sizeof(arr[0]);
+       cout<<row<<endl;
+
+       int column = sizeof(arr[0])/ sizeof(arr[0][0]);
+       cout<<column<<endl;
+       for (int i = 0; i < row; i++)
+    {
+       for (int j = 0; j < column; j++)
+       {
+        
+        cin>>arr[i][j];
+       }
+       
     }
     
-
+    }
+    
+   
+    
 
 
 
